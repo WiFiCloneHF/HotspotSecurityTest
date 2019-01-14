e0fc1decb35baaf975de86f73f591689428a4f92
WiFiCloneHF <46676695+WiFiCloneHF@users.noreply.github.com>
2019-01-15 01:49:13 +1300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
-mv hostapd.conf /root/home/hostapd.conf
-mv dnsmasq.conf /root/home/hostapd.conf
+mv /root/HotspotSecurityTest/hostapd.conf /root/home/hostapd.conf
+mv /root/HotspotSecurityTest/dnsmasq.conf /root/home/hostapd.conf
 apt-get install hostapd
 ifconfig wlan0mon up 192.168.1.1 netmask 255.255.255.0
 route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.1
-mv 000-default.conf /etc/apache2/sites-available/000-default.conf
-mv index.html /var/www/html/index.html
-mv hotspot-detect.html /var/www/html/hotspot-detect.html
-mv redirect /var/www/html/redirect
-mv process.php /var/www/html/process.php
-mv success.html /var/www/html/success.html
-mv got.txt /var/www/html/got.txt
+mv /root/HotspotSecurityTest/000-default.conf /etc/apache2/sites-available/000-default.conf
+mv /root/HotspotSecurityTest/index.html /var/www/html/index.html
+mv /root/HotspotSecurityTest/hotspot-detect.html /var/www/html/hotspot-detect.html
+mv /root/HotspotSecurityTest/redirect /var/www/html/redirect
+mv /root/HotspotSecurityTest/process.php /var/www/html/process.php
+mv /root/HotspotSecurityTest/success.html /var/www/html/success.html
+mv /root/HotspotSecurityTest/got.txt /var/www/html/got.txt
 chmod a+w /var/www/html/got.txt
 a2enmod php7.2
 systemctl restart apache2
